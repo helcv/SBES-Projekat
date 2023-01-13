d3db547474945ca2aa70e339e50da0409bf0908b
Maric Nikola <nikola1213maric@protonmail.com>
2023-01-13 23:26:21 +0100

    Menu handler

---
+++ FileManager/Client/MenuHandler.cs
@@ -0,0 +1,126 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class MenuHandler
+    {
+        public void InitializeMenu()
+        {
+            Console.WriteLine("Izaberite opciju (unesite broj od 1 do 7)");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("1. Izlistaj folder");
+            Console.WriteLine("2. Procitaj sadrzaj fajla");
+            Console.WriteLine("3. Kreiraj folder");
+            Console.WriteLine("4. Kreiraj fajl");
+            Console.WriteLine("5. Promeni naziv fajla ili foldera");
+            Console.WriteLine("6. Premesti fajl ili folder");
+            Console.WriteLine("7. Obrisi fajl ili folder");
+            Console.WriteLine("0. Napusti program");
+            Console.WriteLine("----------------------------------------------");
+        }
+
+        public bool ReceiveInput(ClientProxy proxy)
+        {
+            bool isFile = false;
+            int option = -1;
+            bool res = false;
+            int.TryParse(Console.ReadLine(), out option);
+
+            if (option == -1 || option > 7)
+            {
+                Console.WriteLine("Nepravilan unos, pokusajte ponovo.");
+            }
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("Da li ste sigurno da zelite da napustite program? (DA/NE)");
+                    string answer = Console.ReadLine();
+                    if (string.Equals(answer, "DA"))
+                    {
+                        return false;
+
+                    }
+                    if (string.Equals(answer, "NE"))
+                    {
+                        InitializeMenu();
+                    }
+                    Console.WriteLine("Pogresan unos, probajte ponovo.");
+                    InitializeMenu();
+                    break;
+                case 1:
+                    Console.WriteLine("Unesite naziv foldera");
+                    string folderContents = proxy.ShowFolder(Console.ReadLine());
+                    Console.Write($"Sadrzaj foldera: {Environment.NewLine} {folderContents} {Environment.NewLine}");
+                    break;
+                case 2:
+                    Console.WriteLine("Unesite naziv fajla");
+                    string fileContents = proxy.ReadFile(Console.ReadLine());
+                    Console.Write($"Sadrzaj fajla: {Environment.NewLine} {fileContents} {Environment.NewLine}");
+                    break;
+                case 3:
+                    Console.WriteLine("Unesite naziv foldera koji zelite da kreirate");
+                    res = proxy.CreateFolder(Console.ReadLine());
+                    if (res) Console.WriteLine($" {Environment.NewLine} Uspesno kreiran folder");
+
+                    break;
+                case 4:
+                    Console.WriteLine("Unesite naziv fajla koji zelite da kreirate");
+                    string filename = Console.ReadLine();
+                    Console.WriteLine("Unesite sadrzaj fajla");
+                    string content = Console.ReadLine();
+                    res = proxy.CreateFile(filename, content);
+                    if (res) Console.WriteLine($" {Environment.NewLine} Fajl uspesno kreiran");
+                    break;
+                case 5:
+                    // to do promeni naziv fajla
+                    break;
+                case 6:
+                    // to do premesti fajl
+                    break;
+                case 7:
+                    try
+                    {
+                        isFile = IsFileOrFolder();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        break;
+                    }
+                    Console.WriteLine("Unesite naziv fajla ili foldera koji zelite da obrisete");
+                    Console.WriteLine("UPOZORENJE: Ukoliko brisete folder, obrisace se i svi fajlovi u njemu!");
+                    string name = Console.ReadLine();
+                    if (name.Contains("."))
+                    {
+                        isFile = true;
+                    }
+                    res = proxy.DeleteFile(name, isFile);
+                    if (res) Console.WriteLine($" {Environment.NewLine} Fajl/folder uspesno obrisan");
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+
+        public bool IsFileOrFolder()
+        {
+            Console.WriteLine("Ukoliko zelite da izvrsite operaciju nad fajlom, upisite slovo F");
+            Console.WriteLine("Ukoliko zelite da izvrsite operaciju nad folderom, upisite slovo D");
+            string fileOrFolder = Console.ReadLine();
+            if (!fileOrFolder.Equals("F") && !fileOrFolder.Equals("D"))
+            {
+                throw new Exception("Pogresan unos, pokusajte ponovo");
+            }
+            if (fileOrFolder.Equals("F"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- FileManager/Server/FileSystemManagerService.cs
+++ FileManager/Server/FileSystemManagerService.cs
@@ -19,19 +19,166 @@ namespace Server
         private string currentPath = Path.Combine(Directory.GetCurrentDirectory(), "root");
         public bool CreateFile(string filename, string fileContent)
         {
-            throw new NotImplementedException();
+           CustomPrincipal principal = new CustomPrincipal(ServiceSecurityContext.Current.PrimaryIdentity);
+
+            if (principal.IsInRole("Editor"))
+            {
+                if (string.IsNullOrEmpty(filename))
+                {
+                    try
+                    {
+                        Audit.CreateFailed(filename, "No file name provided.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+
+                    throw new FaultException("Niste uneli naziv fajla!");
+                }
+
+                string pathString = Path.Combine(currentPath, filename);
+                if (File.Exists(pathString))
+                {
+                    try
+                    {
+                        Audit.CreateFailed(filename, "File already exists.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    throw new FaultException("Vec postoji fajl sa unetim nazivom!");
+                }
+                using (StreamWriter sw = File.CreateText(pathString))
+                {
+                    sw.WriteLine(EncryptionManager.DecryptMessage(fileContent));
+
+                }
+                try
+                {
+                    Audit.CreatedSuccessfully(filename, "File successfully created");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                return true;
+            }
+            else
+            {
+                throw new FaultException("Nemate pristup ovoj opciji");
+
+            }
 
         }
       
         public bool CreateFolder(string folderName)
         {
-            throw new NotImplementedException();
+            CustomPrincipal principal = new CustomPrincipal(ServiceSecurityContext.Current.PrimaryIdentity);
+
+            if (principal.IsInRole("Editor"))
+            {
+                if (string.IsNullOrEmpty(folderName))
+                {
+                    try
+                    {
+                        Audit.CreateFailed(folderName, "No folder name provided");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    throw new FaultException("Niste uneli naziv foldera!");
+
+
+                }
+
+                string pathString = Path.Combine(currentPath, folderName);
+                if (Directory.Exists(pathString))
+                {
+                    try
+                    {
+                        Audit.CreateFailed(folderName, "Folder already exists");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                    throw new FaultException("Folder sa unetim nazivom vec postoji!");
+                }
+                Directory.CreateDirectory(pathString);
+
+                try
+                {
+                    Audit.CreatedSuccessfully(folderName, "Folder successfully created");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                return true;
+            }
+            else
+            {
+                throw new FaultException("Nemate pristup ovoj opciji");
+
+            }
 
         }
 
         public bool DeleteFile(string filename, bool isFile)
         {
-            throw new NotImplementedException();
+            CustomPrincipal principal = new CustomPrincipal(ServiceSecurityContext.Current.PrimaryIdentity);
+
+
+            if (principal.IsInRole("Editor"))
+            {
+                string pathString = Path.Combine(currentPath, filename);
+                if (isFile)
+                {
+                    if (!File.Exists(pathString))
+                    {
+                        throw new FaultException("Ne postoji fajl sa unetim nazivom!");
+
+                    }
+                    try
+                    {
+                        File.Delete(pathString);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    if (!Directory.Exists(pathString))
+                    {
+                        throw new FaultException("Ne postoji folder sa unetim nazivom!");
+
+                    }
+                    try
+                    {
+                        Directory.Delete(pathString, true);
+                    }
+                    catch (IOException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                throw new FaultException("Nemate pristup ovoj opciji");
+
+            }
 
         }
 
@@ -43,7 +190,36 @@ namespace Server
 
         public string ReadFile(string filename)
         {
-            throw new NotImplementedException();
+            CustomPrincipal principal = new CustomPrincipal(ServiceSecurityContext.Current.PrimaryIdentity);
+
+
+            if (principal.IsInRole("Viewer") || principal.IsInRole("Editor"))
+            {
+                if (string.IsNullOrEmpty(filename))
+                {
+                    throw new FaultException("Niste uneli naziv fajla!");
+                }
+                string pathString = Path.Combine(currentPath, filename);
+                if (!File.Exists(pathString))
+                {
+                    throw new FaultException("Ne postoji fajl sa unetim nazivom!");
+                }
+
+                string[] lines = File.ReadAllLines(pathString);
+
+                StringBuilder builder = new StringBuilder();
+                foreach (string value in lines)
+                {
+                    builder.Append(value);
+                    builder.Append(Environment.NewLine);
+                }
+
+                return EncryptionManager.EncryptMessage(builder.ToString());
+            }
+            else
+            {
+                throw new FaultException("Nemate pristup ovoj opciji");
+            }
 
         }
         public bool RenameFileOrFolder(string oldName, string newName, bool isFile)
@@ -54,7 +230,32 @@ namespace Server
       
         public string ShowFolder(string filename)
         {
-            throw new NotImplementedException();
+            CustomPrincipal principal = new CustomPrincipal(ServiceSecurityContext.Current.PrimaryIdentity);
+
+
+            if (principal.IsInRole("Viewer") || principal.IsInRole("Editor"))
+            {
+                string pathString = Path.Combine(currentPath, filename);
+
+                DirectoryInfo di = new DirectoryInfo(pathString);
+                if (!di.Exists)
+                {
+                    throw new FaultException("Folder sa unetim nazivom ne postoji!");
+                }
+                string[] files = Directory.GetFileSystemEntries(pathString, "*", SearchOption.AllDirectories);
+                StringBuilder builder = new StringBuilder();
+                foreach (string value in files)
+                {
+                    builder.Append(value);
+                    builder.Append(Environment.NewLine);
+                }
+                return builder.ToString();
+            }
+            else
+            {
+                throw new FaultException("Nemate pristup ovoj opciji");
+            }
+         
         }
 
        
